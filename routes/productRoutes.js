ca59ccf7389a9fc7b8152c6bc1d7f146a8a3288e
Abdlrhman00 <133212657+Abdlrhman00@users.noreply.github.com>
2025-06-15 18:48:28 +0300

    search filters

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -15,9 +15,9 @@ router.get("/search", asyncHandler(productController.searchProducts));
 router.post("/search-by-image", upload.single("image"), asyncHandler(productController.searchByImage));
 router.get("/popular", getPopularProducts);
 router.get("/discounts", getDiscountedProducts);
-router.get("/:id", asyncHandler(productController.getProductById));
 router.put("/:productId/view", incrementViewCount);
 router.get("/searchFilters", searchFilters)
 router.post("/creatProduct");
+router.get("/:id", asyncHandler(productController.getProductById));
 
 module.exports = router;
